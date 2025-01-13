1c75f4f1ee2885b1908d9e974027d7d107e17164
Niyetkhan <nietxan.s@gmail.com>
2025-01-13 10:40:50 +0500

    telescope node_modules & pulumi

--- neovim/dot-config/nvim/after/plugin/telescope.lua
+++ neovim/dot-config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,11 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+
+require('telescope').setup{ 
+	defaults = { 
+		file_ignore_patterns = { 
+			"node_modules" 
+		}
+	}
+}
