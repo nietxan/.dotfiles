06a534da499dffe369839876193c044f8cea3e77
nietxan <niyetkhan.saduakassov@qcloudy.io>
2025-02-20 08:47:11 +0500

    $editor, oil conf and depth of sessionizer

--- neovim/dot-config/nvim/after/plugin/oil.lua
+++ neovim/dot-config/nvim/after/plugin/oil.lua
@@ -1,5 +1,19 @@
 require("oil").setup({
+	keymaps = {
+		["<C-p>"] = "actions.preview",
+		["gx"] = "actions.open_external",
+		["_"] = { "actions.open_cwd", mode = "n" },
+	},
+	columns = {
+		"icon",
+		"permissions",
+		"mtime"
+	},
 	view_options = {
 		show_hidden = true,
-	}
+	},
+	confirmation = {
+		border = "single",
+	},
+	skip_confirm_for_simple_edits = false,
 })
