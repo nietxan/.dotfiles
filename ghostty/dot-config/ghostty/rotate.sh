a3dd7a5b995af864fe7ae4d197c90b23971b6f79
Niyetkhan <nietxan.s@gmail.com>
2026-06-29 22:16:50 +0500

    some path fixes

--- ghostty/dot-config/ghostty/rotate.sh
+++ ghostty/dot-config/ghostty/rotate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PATH=/opt/local/bin:/opt/local/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/bin
+
 # */5 * * * *
 ln -sf $(find $HOME/.local/share/terminal-bg -type f | shuf -n 1) \
 	$HOME/.local/share/terminal-bg/lock.jpg
