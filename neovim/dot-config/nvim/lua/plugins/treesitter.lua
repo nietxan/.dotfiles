0ac360fa513bb2acbf339ed60e73eb6881200a41
niyetkhan <nieytkhan@remofirst.com>
2026-08-10 10:51:29 +0500

    some minor fixes

--- neovim/dot-config/nvim/lua/plugins/treesitter.lua
+++ neovim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  -- branch = "main",
   event = { "BufReadPost", "BufNewFile" },
   opts = {
-    ensure_installed = { "python", "cpp", "c", "go", "lua", "typescript" },
-    sync_install = false,
+    ensure_installed = { "python", "cpp", "c", "go", "lua", "typescript", "markdown", "markdown_inline" },
     auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
   },
   config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    local TS = require("nvim-treesitter")
+
+    -- Setup options for modern nvim-treesitter
+    if type(TS.setup) == "function" then
+      TS.setup(opts)
+    end
+
+    -- Install configured parsers if TSInstall/install module is available
+    local install_ok, install = pcall(require, "nvim-treesitter.install")
+    if install_ok and type(install.update) == "function" then
+      install.update(opts.ensure_installed)
+    end
   end,
 }
